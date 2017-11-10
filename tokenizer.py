992553cc841c2de330c221aaae9e8a59606d7a2f
GGreenfield <glg995@gmail.com>
2017-11-09 20:58:21 -0500

    development halted as azlyrics.com is offline

--- tokenizer.py
+++ tokenizer.py
@@ -2,8 +2,6 @@
 from bs4 import BeautifulSoup
 import urllib2, random, re
 
-#Obtain list of links
-#Iterate through each link and save lyrics line by link with START and END tokens
 def buildTextData():
 	baseUrl = "https://www.azlyrics.com"
 	artist = "/d/deathgrips.html"
@@ -15,9 +13,20 @@ def buildTextData():
 	for link in songLinks:
 		page = urllib2.urlopen(baseUrl+link.replace('..',''))
 		soup = BeautifulSoup(page)
-		lyrics = [lyric for lyric in soup.find_all('div')]
-		print(lyrics[21])
 
+		lyrics = str(soup)
+		startFlag = '<!-- Usage of azlyrics.com content by any third-party lyrics provider is prohibited by our licensing agreement. Sorry about that. -->'
+		lyrics = lyrics.split(startFlag)[1]
+		lyrics = lyrics.split('</div>')[0]
+		##now replace the html tags and strip
+		##for each line add a START and END tag
+		##write each line to a file locally
+		print(lyrics)
+
+	
+#this script only needs to be run once(or whenever the artist releases new music)
+#in order to build the body of text from which a frequency table will be built
+#and then a markov chain will be implemented
 #main
 buildTextData()
 
